93f03bc0208af7ec3ae758100b32a9ce51d7c5d2
Fenech <max1.mmm@yandex.ru>
2023-02-21 18:19:46 +0300

    Add list

--- ManagmentEmploye/MainWindow.xaml.cs
+++ ManagmentEmploye/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RestSharp;
 
 namespace ManagmentEmploye
 {
@@ -23,6 +27,7 @@ namespace ManagmentEmploye
         public MainWindow()
         {
             InitializeComponent();
+            GetEmployees();
         }
 
         private void Help(object sender, RoutedEventArgs e)
@@ -30,19 +35,36 @@ namespace ManagmentEmploye
 
         }
 
+        private void GetEmployees()
+        {
+            var client = new RestClient("http://f0772709.xsph.ru/API");
+            var request = new RestRequest("/GetEmployees.php");
+            RestResponse response = client.Execute(request);
+            string json = response.Content.ToString();
+            ObservableCollection<Employee> employees = JsonSerializer.Deserialize<ObservableCollection<Employee>>(json);
+            ListEmployee.ItemsSource = employees;
+            foreach (var item in employees)
+            {
+
+            }
+        }
+
         private void MinimazeButton(object sender, RoutedEventArgs e)
         {
-
+            this.WindowState = WindowState.Minimized;
         }
 
         private void MaximizeButton(object sender, RoutedEventArgs e)
         {
-
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else
+                this.WindowState = WindowState.Normal;
         }
 
         private void CloseButton(object sender, RoutedEventArgs e)
         {
-
+            Application.Current.Shutdown();
         }
 
         private void Add(object sender, RoutedEventArgs e)
@@ -56,13 +78,36 @@ namespace ManagmentEmploye
         }
 
         private void EditButton(object sender, RoutedEventArgs e)
-        {
+        { 
 
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void LeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Remove(object sender, RoutedEventArgs e)
+        {
+            
+            if (ListEmployee.SelectedItems.Count > 0)
+            {
+                Employee emp = (Employee)((Button)sender).DataContext;
+
+                var client = new RestClient("http://f0772709.xsph.ru/API");
+                var request = new RestRequest($"/RemoveEmploye.php?id={emp.Id}");
+                //WebClient webClient = new WebClient()
+                //request.AddParameter("id", emp.Id);
+                client.Execute(request);
+                ListEmployee.ItemsSource = null;
+                ListEmployee.Items.Remove(emp);
+                GetEmployees();
+            }
         }
     }
 }
